55f85fcd503b3c1f1a320f52765ef5257ddd273f
yuanyulou <434356430@qq.com>
2019-05-14 13:22:49 +0800

    新建springboot项目

---
+++ src/main/java/cn/itlou/somebootfunc/SomeBootFuncApplication.java
@@ -0,0 +1,13 @@
+package cn.itlou.somebootfunc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeBootFuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeBootFuncApplication.class, args);
+	}
+
+}
